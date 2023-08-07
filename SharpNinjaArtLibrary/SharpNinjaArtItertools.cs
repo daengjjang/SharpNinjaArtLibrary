840e107fd1640a1c85d3e1547940aa5212cc7596
댕짱 <139039103+daengjjang@users.noreply.github.com>
2023-08-08 03:10:38 +0900

    Update SharpNinjaArtItertools.cs
    Select Many 형식 수정

--- SharpNinjaArtLibrary/SharpNinjaArtItertools.cs
+++ SharpNinjaArtLibrary/SharpNinjaArtItertools.cs
@@ -9,25 +9,28 @@ public static class SharpNinjaArtItertools
             Aggregate(
                 new[] { Enumerable.Empty<T>() } as IEnumerable<IEnumerable<T>>, 
                 (accumulator, seq) => 
-                    accumulator.SelectMany(_ => seq, (accseq, item) => accseq.Concat(new[] { item })));
+                    accumulator.SelectMany(_ => seq, (e, seq) => e.Concat(new[] { seq })));
     }
+
     public static IEnumerable<IEnumerable<T>> Permutations<T>(IEnumerable<T> list, int length)
     {
         var enumerable = list.ToList();
         return length == 0 ? 
             new []{ Array.Empty<T>() } :
-            enumerable.SelectMany((e, i) => 
-                Permutations(enumerable.Where((_, j) => i != j), length - 1)
-                    .Select(s => new[] { e }.Concat(s))
-                );
+            enumerable.SelectMany((_, i) => 
+                    Permutations(enumerable.Where((_, j) => i != j), length - 1) , 
+                (e ,per) => new [] {e}.Concat(per)
+            );
     }
+
     public static IEnumerable<IEnumerable<T>> Combinations<T>(IEnumerable<T> list, int length, bool allowReplacement)
     {
         var enumerable = list.ToList();
-        return length == 0 ? 
-            new[] {Array.Empty<T>()} :
-            enumerable.SelectMany((e, i) =>
-                Combinations(enumerable.Skip(i + (allowReplacement ? 0 : 1) ), length - 1, allowReplacement)
-                    .Select(s => new[] {e}.Concat(s)));
+        return length == 0
+            ? new[] {Array.Empty<T>()}
+            : enumerable.SelectMany((_, i) =>
+                    Combinations(enumerable.Skip(i + (allowReplacement ? 0 : 1)), length - 1, allowReplacement),
+                (e, comb) => new[] {e}.Concat(comb)
+            );
     }
 }
