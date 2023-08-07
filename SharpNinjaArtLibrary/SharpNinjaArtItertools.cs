3ec0692aeeac09d79f78ad2aca4829b0cd8d863e
댕짱 <139039103+daengjjang@users.noreply.github.com>
2023-08-08 01:25:12 +0900

    Update SharpNinjaArtItertools.cs
    양식수정

--- SharpNinjaArtLibrary/SharpNinjaArtItertools.cs
+++ SharpNinjaArtLibrary/SharpNinjaArtItertools.cs
@@ -5,7 +5,8 @@ public static class SharpNinjaArtItertools
     public static IEnumerable<IEnumerable<T>> Permutations<T>(IEnumerable<T> list, int length)
     {
         var enumerable = list.ToList();
-        return length == 0 ? new []{ Array.Empty<T>() } :
+        return length == 0 ? 
+            new []{ Array.Empty<T>() } :
             enumerable.SelectMany((e, i) => 
                 Permutations(enumerable.Where((_, j) => i != j), length - 1)
                     .Select(s => new[] { e }.Concat(s))
@@ -14,9 +15,9 @@ public static class SharpNinjaArtItertools
     public static IEnumerable<IEnumerable<T>> Combinations<T>(IEnumerable<T> list, int length, bool allowReplacement)
     {
         var enumerable = list.ToList();
-        return length == 0
-            ? new[] {Array.Empty<T>()}
-            : enumerable.SelectMany((e, i) =>
+        return length == 0 ? 
+            new[] {Array.Empty<T>()} :
+            enumerable.SelectMany((e, i) =>
                 Combinations(enumerable.Skip(i + (allowReplacement ? 0 : 1) ), length - 1, allowReplacement)
                     .Select(s => new[] {e}.Concat(s)));
     }
